1ddce4fbfd4d504309f23b139cf23a57621779f3
Saul <chavez5s18@gmail.com>
2016-09-26 00:22:39 -0500

    new file:   CampusConnection/.gitignore 	new file:   CampusConnection/.idea/compiler.xml 	new file:   CampusConnection/.idea/copyright/profiles_settings.xml 	new file:   CampusConnection/.idea/gradle.xml 	new file:   CampusConnection/.idea/misc.xml 	new file:   CampusConnection/.idea/modules.xml 	new file:   CampusConnection/.idea/runConfigurations.xml 	new file:   CampusConnection/app/.gitignore 	new file:   CampusConnection/app/build.gradle 	new file:   CampusConnection/app/proguard-rules.pro 	new file:   CampusConnection/app/src/androidTest/java/com/codingcrew/campusconnection/ExampleInstrumentedTest.java 	new file:   CampusConnection/app/src/main/AndroidManifest.xml 	new file:   CampusConnection/app/src/main/java/com/codingcrew/campusconnection/LoginActivity.java 	new file:   CampusConnection/app/src/main/java/com/codingcrew/campusconnection/User.java 	new file:   CampusConnection/app/src/main/res/drawable/button.xml 	new file:   CampusConnection/app/src/main/res/layout/activity_login.xml 	new file:   CampusConnection/app/src/main/res/mipmap-hdpi/ic_launcher.png 	new file:   CampusConnection/app/src/main/res/mipmap-mdpi/ic_launcher.png 	new file:   CampusConnection/app/src/main/res/mipmap-xhdpi/ic_launcher.png 	new file:   CampusConnection/app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   CampusConnection/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	new file:   CampusConnection/app/src/main/res/values-w820dp/dimens.xml 	new file:   CampusConnection/app/src/main/res/values/colors.xml 	new file:   CampusConnection/app/src/main/res/values/dimens.xml 	new file:   CampusConnection/app/src/main/res/values/strings.xml 	new file:   CampusConnection/app/src/main/res/values/styles.xml 	new file:   CampusConnection/app/src/test/java/com/codingcrew/campusconnection/ExampleUnitTest.java 	new file:   CampusConnection/build.gradle 	new file:   CampusConnection/gradle.properties 	new file:   CampusConnection/gradle/wrapper/gradle-wrapper.jar 	new file:   CampusConnection/gradle/wrapper/gradle-wrapper.properties 	new file:   CampusConnection/gradlew 	new file:   CampusConnection/gradlew.bat 	new file:   CampusConnection/settings.gradle

---
+++ CampusConnection/app/src/main/java/com/codingcrew/campusconnection/LoginActivity.java
@@ -0,0 +1,97 @@
+package com.codingcrew.campusconnection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button mButtonLogin, mButtonRegister;
+    EditText mUsername, mPassword;
+    User mUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.codingcrew.campusconnection.R.layout.activity_login);
+
+        mUser = new User();
+
+        //Link the activity_login.xml attributes to current activity
+        mUsername = (EditText) findViewById(R.id.textViewUsername);
+        mPassword = (EditText) findViewById(R.id.textViewPassword);
+
+        //Store the user name into a string variable
+        mUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mUser.setUsername(s.toString());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+      // Store the user password into a string variable
+        mPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mUser.setPassword(s.toString());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        // Link the activity_login.xml attributes to current activity
+        mButtonLogin = (Button) findViewById(R.id.buttonLogin);
+        mButtonRegister = (Button) findViewById(R.id.buttonRegister);
+
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Toast toast = Toast.makeText(getApplicationContext(), "Welcome " + mUser.getUsername() +
+                        " " + mUser.getPassword()
+                        , Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                Toast toast = Toast.makeText(getApplicationContext(), "Thank you for using our services", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+    }
+}
+
+
+
+
+
+
---
+++ CampusConnection/app/src/main/java/com/codingcrew/campusconnection/User.java
@@ -0,0 +1,31 @@
+package com.codingcrew.campusconnection;
+
+/**
+ * Created by Saul on 9/25/2016.
+ */
+
+public class User {
+    private String mUsername;
+    private String mPassword;
+
+    User(){
+        mPassword = "";
+        mUsername = "";
+    }
+
+    public String getUsername() {
+        return mUsername;
+    }
+
+    public void setUsername(String username) {
+        mUsername = username;
+    }
+
+    public String getPassword() {
+        return mPassword;
+    }
+
+    public void setPassword(String password) {
+        mPassword = password;
+    }
+}
