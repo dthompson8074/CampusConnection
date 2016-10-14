3b72cbf656b44f60b24d640b6a7117ebb81adb00
Garrett James <halon1795@yahoo.com>
2016-10-13 20:28:51 -0500

    Garrett- New Event Page

---
+++ CampusConnection/app/src/main/java/com/codingcrew/campusconnection/newStudyEvent.java
@@ -0,0 +1,13 @@
+package com.codingcrew.campusconnection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newStudyEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_study_event);
+    }
+}
